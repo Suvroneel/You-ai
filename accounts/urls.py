064d4bd135e61dba17630ca8232968d4f76b6f74
Suvroneel Nathak <suvroneelnathak213@gmail.com>
2026-09-16 21:50:54 +0530

    Big step added sidebar

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("nickname/", views.nickname_view, name="nickname"),
+    path("personality/", views.personality_setup_view, name="personality_setup"),
     path("logout/", views.logout_view, name="logout"),
 ]
